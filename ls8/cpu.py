64a12ab556b4313127ff07000088bb089e26cb4e
Chelsea Wetzel <chelswetz@gmail.com>
2020-07-23 02:05:34 -0400

    finish alu MUL

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -28,27 +28,28 @@ class CPU:
         # halt the CPU
         exit()
 
+
     def load(self):
         """Load a program into memory."""
 
         address = 0
 
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        if len(sys.argv) != 2:
+            print("usage: comp.py filename")
+            sys.exit(1)
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    try:
+                        line = line.split("#",1)[0]
+                        line = int(line, 2) # int() is base 10 by default, so I had to change it to binary base 2
+                        self.ram[address] = line
+                        address += 1
+                    except ValueError:
+                        pass
+        except FileNotFoundError:
+            print(f"Couldn't find file {sys.argv[1]}")
+            sys.exit(1)
 
 
     def alu(self, op, reg_a, reg_b):
@@ -56,7 +57,12 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] = self.reg[reg_a] * self.reg[reg_b]
+            # Questions for bub:
+            # how can I store the result in a parameter? 
+            # do I use both of the registers that we pass in? 
+            # do I need to call alu somewhere and pass in the right registers? 
         else:
             raise Exception("Unsupported ALU operation")
 
